25ee16e2a4063898cfe0aefd13bc28fceb78ba0c
marck1391 <marck1391@gmail.com>
2015-02-20 21:20:43 -0600

    updated

--- _sources/socket.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-title ChatServer
-bamc socket.bcp
-copy socket.exe /b "../socket.exe" /b
-pause
-cls
--- _sources/socket.php
+++
@@ -1,8 +0,0 @@
-<?php
-error_reporting(E_ALL);
-set_time_limit(0);
-ob_implicit_flush();
-
-include "Classes/SocketServer.php";
-$chat = new SocketServer("192.168.2.2", 65500);
-?> 
